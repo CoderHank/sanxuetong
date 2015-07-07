30ba33b6e70eb8637aa9e1c415cdc34c0b9e7b58
CoderHank <tomexhj@gmail.com>
2015-07-07 18:19:23 +0800

    新增Rest方法

--- src/main/java/com/desd/controller/ForumController.java
+++ src/main/java/com/desd/controller/ForumController.java
@@ -1,5 +1,38 @@
 package com.desd.controller;
 
-public class ForumController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/forum")
+public class ForumController {
+	/**
+	 * 获取所有主题
+	 * @return
+	 */
+	@RequestMapping(value="/subjects", method=RequestMethod.GET)
+	@ResponseBody
+	public String getAllSubjects(){
+		return null;
+	}
+	
+	/**
+	 * 根据ID获取特定主题
+	 * @param subject_id
+	 * @return
+	 */
+	@RequestMapping(value="/subject", method=RequestMethod.GET)
+	@ResponseBody
+	public String getSubjectById(@RequestParam("subject_id") long subject_id){
+		return null;
+	}
+	
+	@RequestMapping(value="/posts", method=RequestMethod.GET)
+	@ResponseBody
+	public String getPostsBySubjectId(@RequestParam("subject_id") long subject_id){
+		return null;
+	}
 }
